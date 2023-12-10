b4032c2fffb5419f4884d7175894f86eacb6cca9
Edzz <edusuuu@user.noreply.github.com>
2023-12-10 19:19:01 +0800

    dev branch

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,13 @@
+import "./navbar.css"
+export default function Navbar() {
+    return (
+        <ul class="navbar">
+            <li class="links"><a>home</a></li>
+            <li class="links"><a>products</a></li>
+            <li class="links"><a>map</a></li>
+            <li class="links"><a>payment method</a></li>
+            <li class="links"><a>about us</a></li>
+            <li class="links"><a>sign in</a></li>
+        </ul>
+    ) 
+}
